9fd98ee0848abc5248d6750c2855d664b4e29eba
startupmarian <startup.marian@gmail.com>
2013-07-13 22:27:18 +0000

    update

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
+#!/usr/bin/env node
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buf = new Buffer(fs.readFileSync('index.html'));
+  var buf = new Buffer(fs.readFileSync('index.html'), 'utf8');
 
   response.send(buf.ToString('utf8'));
 });
