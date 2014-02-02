7854abd91ece8e2e4fe4230ddaa449c5ce52992a
vagrant <vagrant@precise32.(none)>
2014-02-02 22:22:26 +0000

    return webjs

--- web.js
+++ web.js
@@ -35,7 +35,7 @@ app.get('/', function(request, response) {
 
 
 // language/es
-app.get('/es', function(request, response) {
+app.get('/language/es', function(request, response) {
    gettext.setlocale('LC_ALL', 'es');
    response.redirect("/");
 });
