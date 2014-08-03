4c68521335d65d1f9e3ae8798b1488e0bd8e4ca5
Marianna <startup.marian@gmail.com>
2014-08-03 00:11:24 +0000

    setup webjs

--- web.js
+++ web.js
@@ -26,7 +26,7 @@ app.use(i18n.abide({
 
 // Render homepage
 app.get('/', function(request, response) {
-   request.setLocale('en');
+//   request.setLocale('en');
    response.render("homepage", {
 	title: request.gettext("From the body toward the light. Translation into Russian"),
         lang: 'en'
